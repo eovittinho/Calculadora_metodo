3313128c03f1de38c2168c4d198d22e3ef514571
eovittinho <victorthulio001@gmail.com>
2025-02-04 16:11:34 -0300

    Adição

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,15 @@
+namespace Calculadora
+{
+    class Operacoes
+    {
+     public static void RealizarAdicao(
+        decimal valor1,
+        decimal valor2
+        )
+        {
+         decimal soma = valor1 + valor2;
+         Console.WriteLine($"A soma dos valores {valor1} e {valor2} é: {soma}");
+        }
+
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,18 @@
+﻿using Calculadora;
+
+Console.Clear(); 
+
+repetir:
+Console.WriteLine($"Insira o primeiro valor:");
+if(!decimal.TryParse(Console.ReadLine(), out decimal valor1))
+{
+Console.WriteLine("Valor Inválido! ");
+goto repetir;
+}
+Console.WriteLine($"Insira o segundo valor: ");
+if(!decimal.TryParse(Console.ReadLine(), out decimal valor2))
+{
+Console.WriteLine("Valor Inválido! ");
+goto repetir;
+}
+Operacoes.RealizarAdicao(valor1, valor2);

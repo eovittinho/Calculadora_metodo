86028085d19d3824d0ffc50b305d6dd0cedb7b0b
eovittinho <victorthulio001@gmail.com>
2025-02-05 14:41:07 -0300

    Calculadora: adição

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,15 +1,27 @@
 namespace Calculadora
 {
-    class Operacoes
-    {
-     public static void RealizarAdicao(
-        decimal valor1,
-        decimal valor2
-        )
-        {
-         decimal soma = valor1 + valor2;
-         Console.WriteLine($"A soma dos valores {valor1} e {valor2} é: {soma}");
-        }
 
-    }
+   class Operacoes
+   {
+      
+      public static decimal ObterValores()
+      {
+      repetir:
+          Console.Write("Informe o valor: ");
+          if (!decimal.TryParse(Console.ReadLine(), out decimal valor))
+          {
+            Console.WriteLine("Valor inválido! ");
+            goto repetir;
+          }
+          else
+          {
+            return valor;
+          }
+      }
+      public static void RealizarAdicao(decimal valor1, decimal valor2)
+      {
+        Console.WriteLine($"A soma dos valores é: {valor1 + valor2} ");
+      }
+   }
+
 }
